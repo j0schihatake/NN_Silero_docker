38fb1d2e0410271bdce6f6036066f31568a7023c
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-26 18:59:14 +0300

    Добавил проект

---
+++ src/main/java/com/example/nn_silero/NnSileroApplication.java
@@ -0,0 +1,13 @@
+package com.example.nn_silero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnSileroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnSileroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nn_silero/NnSileroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nn_silero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnSileroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
